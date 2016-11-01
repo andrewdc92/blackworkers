947e642547281a4705f7c351fb9edaab4f896512
andrewdc92 <andrew.cordivari@gmail.com>
2016-10-31 18:28:01 -0700

    added view for project inspiration

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
     root 'landing#index'
 
+    get '/theidea', to: 'landing#idea', as: 'idea'
+    get '/theproject', to: 'landing#project', as: 'project'
+
+
     get '/login', to: 'sessions#new', as: "login"
     post '/sessions', to: 'sessions#create', as: "sessions"
     get '/logout', to: 'sessions#destroy', as: "logout"
+
 
     get "/users", to: "users#index", as: "users"
     get "/users/new", to: "users#new", as: "new_user"
