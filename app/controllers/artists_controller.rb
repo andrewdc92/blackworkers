92d2474f671293f3771058d7925346e2b0de29e4
andrewdc92 <andrew.cordivari@gmail.com>
2016-11-01 23:57:57 -0700

    prepping to drop and reseed db

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,15 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def show
+    @artist = Artist.find_by_id(params[:id])
+  end
+
 end
