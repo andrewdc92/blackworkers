6a2f50c02bf9c0f6410472694306d951688b2ae4
andrewdc92 <andrew.cordivari@gmail.com>
2016-11-02 19:45:48 -0700

    getting ready to run migration, adding artist_name to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-  has_many :users
-  has_many :artists
-  belongs_to :user, optional: true
-  belongs_to :artist, optional: true
+  # has_many :users
+  # belongs_to :user, optional: true
+  belongs_to :artist
 end
